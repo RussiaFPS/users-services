9a315eb44108bc590b867e0dec3e0446e4f86b11
n nikita <nikitakrut58@gmail.com>
2023-10-22 00:00:09 +0300

    controller add route

--- internal/controller/http/router.go
+++ internal/controller/http/router.go
@@ -1,8 +1,13 @@
 package http
 
 import (
+	"encoding/json"
 	"github.com/gin-gonic/gin"
+	"io/ioutil"
+	"log"
+	"net/http"
 	"users-services/internal/repo"
+	"users-services/internal/types"
 )
 
 type Controller struct {
@@ -22,5 +27,102 @@ func (c Controller) NewRoute() {
 }
 
 func (c Controller) add(ctx *gin.Context) {
-	//TODO add
+	jsonData, err := ioutil.ReadAll(ctx.Request.Body)
+	if err != nil {
+		log.Println("Add read request body err: ", err)
+		return
+	}
+	user := types.UsersReq{}
+	err = json.Unmarshal(jsonData, &user)
+	if err != nil {
+		log.Println("Add unmarshal request body err: ", err)
+		return
+	}
+	if user.Name == "" || user.Surname == "" {
+		log.Println("Add empty fields: name or surname")
+		ctx.Status(http.StatusNotFound)
+		return
+	}
+
+	// Request Age API
+	req, err := http.Get(types.AgeAPI + user.Name)
+	if err != nil {
+		log.Println("Error get age API: ", err)
+		ctx.Status(http.StatusServiceUnavailable)
+		return
+	}
+
+	defer req.Body.Close()
+	body, err := ioutil.ReadAll(req.Body)
+	if err != nil {
+		log.Println("Error read body age API: ", err)
+		ctx.Status(http.StatusServiceUnavailable)
+		return
+	}
+
+	userAgeApi := &types.UserAgeApi{}
+	err = json.Unmarshal(body, &userAgeApi)
+	if err != nil {
+		log.Println("Error unmarshal body age API: ", err)
+		ctx.Status(http.StatusServiceUnavailable)
+		return
+	}
+
+	// Request Gender API
+	req, err = http.Get(types.GenAPI + user.Name)
+	if err != nil {
+		log.Println("Error get gender API: ", err)
+		ctx.Status(http.StatusServiceUnavailable)
+		return
+	}
+
+	defer req.Body.Close()
+	body, err = ioutil.ReadAll(req.Body)
+	if err != nil {
+		log.Println("Error read body gender API: ", err)
+		ctx.Status(http.StatusServiceUnavailable)
+		return
+	}
+
+	userGenderApi := &types.UserGenderApi{}
+	err = json.Unmarshal(body, &userGenderApi)
+	if err != nil {
+		log.Println("Error unmarshal body gender API: ", err)
+		ctx.Status(http.StatusServiceUnavailable)
+		return
+	}
+
+	// Request Country API
+	req, err = http.Get(types.CountryAPI + user.Name)
+	if err != nil {
+		log.Println("Error get country API: ", err)
+		ctx.Status(http.StatusServiceUnavailable)
+		return
+	}
+
+	defer req.Body.Close()
+	body, err = ioutil.ReadAll(req.Body)
+	if err != nil {
+		log.Println("Error read body country API: ", err)
+		ctx.Status(http.StatusServiceUnavailable)
+		return
+	}
+
+	userCountryApi := &types.UserCountryApi{}
+	err = json.Unmarshal(body, &userCountryApi)
+	if err != nil {
+		log.Println("Error unmarshal body country API: ", err)
+		ctx.Status(http.StatusServiceUnavailable)
+		return
+	}
+
+	if len(userCountryApi.Country) > 0 {
+		u := types.User{Name: user.Name, Surname: user.Surname, Patronymic: user.Patronymic, Age: userAgeApi.Age,
+			Gender: userGenderApi.Gender, CountryId: userCountryApi.Country[0].CountryId}
+		ctx.JSON(201, u)
+	} else {
+		u := types.User{Name: user.Name, Surname: user.Surname, Patronymic: user.Patronymic, Age: userAgeApi.Age,
+			Gender: userGenderApi.Gender, CountryId: ""}
+		ctx.JSON(201, u)
+	}
 }
--- internal/types/users.go
+++ internal/types/users.go
@@ -2,6 +2,10 @@ package types
 
 import "gorm.io/gorm"
 
+const AgeAPI = "https://api.agify.io/?name="
+const GenAPI = "https://api.genderize.io/?name="
+const CountryAPI = "https://api.nationalize.io/?name="
+
 type User struct {
 	gorm.Model
 	Name       string
@@ -10,4 +14,34 @@ type User struct {
 	Age        int
 	Gender     string
 	CountryId  string
+}
+
+type CountryInfo struct {
+	CountryId   string  `json:"country_id"`
+	Probability float64 `json:"probability"`
+}
+
+type UsersReq struct {
+	Name       string `json:"name"`
+	Surname    string `json:"surname"`
+	Patronymic string `json:"patronymic"`
+}
+
+type UserAgeApi struct {
+	Count int    `json:"count"`
+	Name  string `json:"name"`
+	Age   int    `json:"age"`
+}
+
+type UserGenderApi struct {
+	Count       int     `json:"count"`
+	Name        string  `json:"name"`
+	Gender      string  `json:"gender"`
+	Probability float64 `json:"probability"`
+}
+
+type UserCountryApi struct {
+	Count   int           `json:"count"`
+	Name    string        `json:"name"`
+	Country []CountryInfo `json:"country"`
 }
